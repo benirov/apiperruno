05c6f48e0fb5d67b87ab813e339373b53ab8ad9f
Develov <beniro_vielma@hotmail.com>
2020-02-22 23:23:49 -0500

    make it better

--- src/components/pages/CardInfoDogs.jsx
+++ src/components/pages/CardInfoDogs.jsx
@@ -8,35 +8,113 @@ class CardInfoDogs extends React.Component {
     render(){
         return(
             <div>
-                    <div className="webinar-card">
-		<div className="header-img">
-			<a id="" href="#">
-				<i id="playBtn" className="header-icon icon-play"><img className="btn-circle hederPorfileImg" src="/img/sofia.jpg" alt=""/></i>
-			</a>
-		</div>
-		<div className="webinar-card__content">
-			<span className="label">Pug</span>
-            <span className="label">12 Meses</span>
-            <span className="label">Todas las vacunas</span>
-            <div className="row">
-            <div className="col-6">
-                <h1>Sofia Milagros</h1>
-            </div>
-            <div className="col-6">
-                <div className="col-12">
-                    <button data-title="Agregar a favoritos" className="btn btn-danger btn-circle btn-circle-sm m-1"><img className="imgFavoritos" src="/img/favorito.png" alt=""/></button> 
-                    <button data-title="Ver detalle de mascota" className="btn btn-danger btn-circle btn-circle-sm m-1"><img className="imgFavoritos" src="/img/detailDog.png" alt=""/></button> 
+                <br/>
+            <div className="container-fluid text-center"><h3 className="fontSize15"><strong>Ultimas mascotas agregadas segun tus gustos</strong></h3></div>
+            <div className="container parentDivs">
+                    <div className="webinar-card childDivs">
+                        <div className="header-img">
+                            <a id="" href="#">
+                                <i id="playBtn" className="header-icon icon-play"><img Style="vertical-align: super;" className="btn-circle hederPorfileImg" src="/img/sofia.jpg" alt=""/></i>
+                            </a>
+                        </div>
+                        <div className="webinar-card__content">
+                            <span className="label">Pug</span>
+                            <span className="label">12 Meses</span>
+                            <span className="label">Todas las vacunas</span>
+                            <div className="row">
+                            <div className="col-6">
+                                <h3>Sofia Milagros</h3>
+                            </div>
+                            <div className="col-6 col-md-6">
+                                <div className="col-12" Style="margin-left: 32px;">
+                                    <button data-title="Agregar a favoritos" className="btn btn-danger btn-circle btn-circle-sm m-1"><img className="imgFavoritos" src="/img/favorito.png" alt=""/></button> 
+                                    <button data-title="Ver detalle de mascota" className="btn btn-danger btn-circle btn-circle-sm m-1"><img className="imgFavoritos" src="/img/detailDog.png" alt=""/></button> 
+                                </div>
+                            </div>
+                            </div>
+                            <p>Tu nueva Mascota</p>
+                    </div>
+                    <div className="space"></div>
                 </div>
-            </div>
-            </div>
-			<p>Tu nueva Mascota</p>
-	</div>
-  <div className="space"></div>
+                <div className="webinar-card childDivs">
+                        <div className="header-img">
+                            <a id="" href="#">
+                                <i id="playBtn" className="header-icon icon-play"><img Style="vertical-align: super;" className="btn-circle hederPorfileImg" src="/img/sofia.jpg" alt=""/></i>
+                            </a>
+                        </div>
+                        <div className="webinar-card__content">
+                            <span className="label">Pug</span>
+                            <span className="label">12 Meses</span>
+                            <span className="label">Todas las vacunas</span>
+                            <div className="row">
+                            <div className="col-6">
+                                <h3>Sofia Milagros</h3>
+                            </div>
+                            <div className="col-6 col-md-6">
+                                <div className="col-12" Style="margin-left: 32px;">
+                                    <button data-title="Agregar a favoritos" className="btn btn-danger btn-circle btn-circle-sm m-1"><img className="imgFavoritos" src="/img/favorito.png" alt=""/></button> 
+                                    <button data-title="Ver detalle de mascota" className="btn btn-danger btn-circle btn-circle-sm m-1"><img className="imgFavoritos" src="/img/detailDog.png" alt=""/></button> 
+                                </div>
+                            </div>
+                            </div>
+                            <p>Tu nueva Mascota</p>
+                    </div>
+                    <div className="space"></div>
+                </div>
+                <div className="webinar-card childDivs">
+                        <div className="header-img">
+                            <a id="" href="#">
+                                <i id="playBtn" className="header-icon icon-play"><img Style="vertical-align: super;" className="btn-circle hederPorfileImg" src="/img/sofia.jpg" alt=""/></i>
+                            </a>
+                        </div>
+                        <div className="webinar-card__content">
+                            <span className="label">Pug</span>
+                            <span className="label">12 Meses</span>
+                            <span className="label">Todas las vacunas</span>
+                            <div className="row">
+                            <div className="col-6">
+                                <h3>Sofia Milagros</h3>
+                            </div>
+                            <div className="col-6 col-md-6">
+                                <div className="col-12" Style="margin-left: 32px;">
+                                    <button data-title="Agregar a favoritos" className="btn btn-danger btn-circle btn-circle-sm m-1"><img className="imgFavoritos" src="/img/favorito.png" alt=""/></button> 
+                                    <button data-title="Ver detalle de mascota" className="btn btn-danger btn-circle btn-circle-sm m-1"><img className="imgFavoritos" src="/img/detailDog.png" alt=""/></button> 
+                                </div>
+                            </div>
+                            </div>
+                            <p>Tu nueva Mascota</p>
+                    </div>
+                    <div className="space"></div>
+                </div>
+                <div className="webinar-card childDivs">
+                        <div className="header-img">
+                            <a id="" href="#">
+                                <i id="playBtn" className="header-icon icon-play"><img Style="vertical-align: super;" className="btn-circle hederPorfileImg" src="/img/sofia.jpg" alt=""/></i>
+                            </a>
+                        </div>
+                        <div className="webinar-card__content">
+                            <span className="label">Pug</span>
+                            <span className="label">12 Meses</span>
+                            <span className="label">Todas las vacunas</span>
+                            <div className="row">
+                            <div className="col-6">
+                                <h3>Sofia Milagros</h3>
+                            </div>
+                            <div className="col-6 col-md-6">
+                                <div className="col-12" Style="margin-left: 32px;">
+                                    <button data-title="Agregar a favoritos" className="btn btn-danger btn-circle btn-circle-sm m-1"><img className="imgFavoritos" src="/img/favorito.png" alt=""/></button> 
+                                    <button data-title="Ver detalle de mascota" className="btn btn-danger btn-circle btn-circle-sm m-1"><img className="imgFavoritos" src="/img/detailDog.png" alt=""/></button> 
+                                </div>
+                            </div>
+                            </div>
+                            <p>Tu nueva Mascota</p>
+                    </div>
+                    <div className="space"></div>
+                </div>
+
+
+
 </div>
-
-
-
-
 </div>
 
         )
